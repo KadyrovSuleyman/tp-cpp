db65807d6349bf5162bd14ba49c1c1bec8e9bb51
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-03-16 23:17:34 +0300

    added Code coverage

--- project/tests/src/test.cpp
+++ project/tests/src/test.cpp
@@ -18,7 +18,7 @@ TEST(first_test, malloc_matrix) {
     free_matrix(t);
 }
 
-TEST(create_matrix, case_1) {
+TEST(is_matrix_equal, case_1) {
 
     FILE* l = fopen("./project/tests/data/case_1/left", "r");
     FILE* r = fopen("./project/tests/data/case_1/right", "r");
@@ -34,6 +34,27 @@ TEST(create_matrix, case_1) {
     fclose(l);
     fclose(r);
 }
+
+// TEST(minor_matrix, case_2) {
+
+//     FILE* input_file = fopen("./project/tests/data/case_2/input", "r");
+//     FILE* output_file = fopen("./project/tests/data/case_2/output", "r");
+
+//     Matrix* input = create_matrix(input_file);
+//     Matrix* output = create_matrix(output_file);
+
+//     Matrix* result = minor_matrix(input, input->rows, input->cols, 1, 1);
+
+//     ASSERT_TRUE(is_matrix_equal(result, output));
+
+//     free_matrix(input);
+//     free_matrix(output);
+//     free_matrix(result);
+
+//     fclose(input_file);
+//     fclose(output_file);
+// }
+
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
