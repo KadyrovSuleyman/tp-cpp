9179e66365fb203bd64a0d07c70a3d9603f82c1c
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-04-01 15:29:55 +0300

    small fixes

--- CMakeBuilding/project/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kadyrov_suleyman/tp-cpp/project/test
-# Build directory: /home/kadyrov_suleyman/tp-cpp/CMakeBuilding/project/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
