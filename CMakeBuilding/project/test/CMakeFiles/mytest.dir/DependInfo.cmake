95de7dbed845be4fa3216f2a45899a5163da52bb
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2021-04-01 15:19:55 +0300

    small fixes

---
+++ CMakeBuilding/project/test/CMakeFiles/mytest.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kadyrov_suleyman/tp-cpp/project/test/src/test.cpp" "/home/kadyrov_suleyman/tp-cpp/CMakeBuilding/project/test/CMakeFiles/mytest.dir/src/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../project/include"
+  "../project/test/src"
+  "../googletest/include"
+  "../googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kadyrov_suleyman/tp-cpp/CMakeBuilding/project/CMakeFiles/parcer_in_series.dir/DependInfo.cmake"
+  "/home/kadyrov_suleyman/tp-cpp/CMakeBuilding/project/CMakeFiles/parcer_in_parallel.dir/DependInfo.cmake"
+  "/home/kadyrov_suleyman/tp-cpp/CMakeBuilding/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeBuilding/project/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kadyrov_suleyman/tp-cpp/project/test
+# Build directory: /home/kadyrov_suleyman/tp-cpp/CMakeBuilding/project/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
